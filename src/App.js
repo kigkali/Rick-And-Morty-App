b33df93e46794f4e6a3c53276c474b8f9f56a345
Edwin Leonardo <edwin.leonardo2121@outlook.com>
2023-06-15 14:57:44 -0700

    primer commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './App.css';
+import Characters from './components/Characters';
+import imageRickMorty from './img/rick-morty.png';
+
+function App() {
+  const  [characters, setCharacters] = useState(null);
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+    // console.log(api)
+    // console.log(characterApi)
+    setCharacters(characterApi.results);
+  }
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        { characters ? (
+          <Characters characters={characters} setCharacters={setCharacters}/>
+        ): (
+          <>
+          <img src={imageRickMorty} alt="Rick & Morty" className="img-home"/>
+          <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
